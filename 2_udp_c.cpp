0320c55db9b50b3233edce2db9f741aa167a0d4f
Renain18 <rena.ainspan@live.biu.ac.il>
2025-11-17 20:11:29 +0200

    cpp UDP commit

--- 2_udp_c.cpp
+++ 2_udp_c.cpp
@@ -9,42 +9,57 @@
 #include <unistd.h>
 #include <string.h>
 
+// shortcut for std code use
+
 using namespace std;
 
 int main() {
-
+    // defining ip and port values
     const char* ip_address = "127.0.0.1";
     const int port_no = 5555;
-
+    // creating socket with IPv4 and UDP definitions
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    // if the socket identifier number is negative= the function failed- it is not valid and returns an error
     if (sock < 0) {
         perror("error creating socket");
     }
 
+    // creating a struct that includes addresses 
     struct sockaddr_in sin;
+    // clearing the memory of the struct sin along its entire length
     memset(&sin, 0, sizeof(sin));
+    // defining the fields 
     sin.sin_family = AF_INET;
     sin.sin_addr.s_addr = inet_addr(ip_address);
     sin.sin_port = htons(port_no);
 
+    // creating char array with the message sent to the server
     char data[] = "hello";
+    // saving the length of the data
     int data_len = sizeof(data);
 
+    // sending data to the server with all the nesassary detaile (IP, UDP, data, etc)
     int sent_bytes = sendto(sock, data, data_len, 0, (struct sockaddr *) &sin, sizeof(sin));
+    // checking that the input from the client was sent to the server
     if (sent_bytes < 0) {
         perror("error writing to socket");
     }
 
+    // creating new struct to recieve the input 
     struct sockaddr_in from;
     unsigned int from_len = sizeof(struct sockaddr_in);
     char buffer[4096];
+
+    //recieveing the information from the server
     int bytes = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *) &from, &from_len);
+
+    // checking that the input from the client was sent to the server
     if (bytes < 0) {
         perror("error reading from socket");
     }
-
+    // printing out the message from the server
     cout << "The server sent: " << buffer << endl;
-
+    // closing socket
     close(sock);
 
     return 0;
--- 2_udp_s.cpp
+++ 2_udp_s.cpp
@@ -1,6 +1,6 @@
-// This is a C++ code for UDP from the side of the client.
+// This is a C++ code for UDP from the side of the server.
 
-
+// importing libraries for i/o, sockets etc.
 #include <iostream>
 #include <sys/socket.h>
 #include <stdio.h>
@@ -9,39 +9,46 @@
 #include <unistd.h>
 #include <string.h>
 
+// shortcut for std code
 using namespace std;
 
 int main() {
-
+// defining port number
     const int server_port = 5555;
-
+// creating socket with IPv4 and UDP definitions
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
+// if the socket identifier number is negative= the function failed- it is not valid and returns an error
     if (sock < 0) {
         perror("error creating socket");
     }
-
+// creating a struct that includes addresses 
     struct sockaddr_in sin;
-
+// clearing the memory of the struct sin along its entire length
     memset(&sin, 0, sizeof(sin));
-
+// defining the fields 
     sin.sin_family = AF_INET;
     sin.sin_addr.s_addr = INADDR_ANY;
+    // htons- converting the humber in host to the number in the netword
     sin.sin_port = htons(server_port);
-
+// binding the socket and the port
     if (bind(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
         perror("error binding to socket");
     }
-
+// creating new struct where the new message info will be saved
     struct sockaddr_in from;
     unsigned int from_len = sizeof(struct sockaddr_in);
+// saving buffer variablr to save the info recieved to echo it back
     char buffer[4096];
+// reading the information from the client
     int bytes = recvfrom(sock, buffer, sizeof(buffer), 0, (struct sockaddr *) &from, &from_len);
+// checking that the input from the client was recieved
     if (bytes < 0) {
         perror("error reading from socket");
     }
 
     cout << "The client sent: " << buffer << endl;
 
+// sending the message back to the client
     int sent_bytes = sendto(sock, buffer, bytes, 0, (struct sockaddr *) &from, sizeof(from));
     if (sent_bytes < 0) {
         perror("error writing to socket");
