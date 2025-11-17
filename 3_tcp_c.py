6d10bc32014a0be958f0916b0eeb6a6030481759
Renain18 <rena.ainspan@live.biu.ac.il>
2025-11-17 22:48:42 +0200

    python TCP commit

--- 3_tcp_c.py
+++ 3_tcp_c.py
@@ -1,15 +1,30 @@
+#This is a python code for TCP from the side of the client.
+
+#importing full socket library
 import socket 
 
+# defining socket using the socket library
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#defining IP number and port for binding
 dest_ip = '127.0.0.1'
 dest_port = 12345
+#connect to the server with the destination IP and Port
 s.connect((dest_ip, dest_port))
 
+# Asking the user for a message, reading the input message, 
+# and saveing it in msg.
 msg = input("Message to send: ")
-while not msg == 'quit':
+#instead of quit i did nox
+#while loop ready to accept text from the client until he writes nox
+while not msg == 'nox':
+    #sending to the client the msg 
     s.send(bytes(msg, 'utf-8'))
+    #saving the data input from the server
     data = s.recv(4096)
+    #printing out the message recieved
     print("Server sent: ", data.decode('utf-8'))
+    #recive input again from the server
     msg = input("Message to send: ")
 
+#closing socket
 s.close()
--- 3_tcp_s.py
+++ 3_tcp_s.py
@@ -1,19 +1,43 @@
+#This is a python code for TCP from the side of the server.
+
+#importing full socket library
 import socket
 
+# defining socket using the socket library
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#defining IP number and port for binding. IP doesn't matter- accept any IP.
 server_ip = ''
 server_port = 12345
+
+#Binding the ip to the port in the server in the touple (src_ip, src_port)
 server.bind((server_ip, server_port))
-server.listen(5)
+#define a limit to the amount of clients waiting in line
+#to connect to the server. 
+#Here the limit was 5, and I changed it to 8
+server.listen(8)
+
+#continuous loop, awaiting contact from the client
 
 while True:
+    # accept the client if there is one in line
+    # recieve the information in unique variables ("waiter") 
+    # accept the client with the following touple of IP and Port#
     client_socket, client_address = server.accept()
+    #printing out the address of the client that was accepted
     print('Connection from: ', client_address)
+    # saving the data in 1024 characters at most.
     data = client_socket.recv(1024)
+    # decode data as long as its version is not utf8.
     while not data.decode('utf-8') == '':
+        # decode data and print it
         print('Received: ', data.decode('utf-8'))
+        #echo the data back to the client
         client_socket.send(data.upper())
+        #recieve the next data from the client
         data = client_socket.recv(1024)
 
+    #print that the client disconnected from the client socket
     print('Client disconnected')
+    
+    #closing the client socket, not the main socket
     client_socket.close()
